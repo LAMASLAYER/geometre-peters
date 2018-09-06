af6a8163af4e29668d311fac83bd8484d6a3970c
ku714 <jerome.wilmart@clearstream.com>
2018-09-06 11:39:46 +0200

    design

--- src/app/materials/dashboard/dashboard.component.ts
+++ src/app/materials/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
@@ -8,23 +8,45 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
+          { cols: 1, rows: 1, url: '../../../assets/banner.jpg' },
+          { cols: 1, rows: 1 , url: '../../../assets/buildings.jpeg',
+            list1: 'Relevé topographique',
+            list2: 'Bornage',
+            list3: 'Division',
+            list4: 'Etude de voirie',
+            list5: 'Etat des lieux',
+            list6: 'Expertise immobilière',
+            list7: 'Implantation de batiment'
+            },
+          { cols: 1, rows: 1 , url: '../../../assets/call.jpg',
+            list1: 'Tel: +32 495 56 02 85',
+            list2: 'Mail: pters.kevin@gmail.com',
+            list3: 'Adresse: Voie de l\'Air Pur 222, 4052 Beaufays'
+          }        ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { cols: 2, rows: 1, url: '../../../assets/banner.jpg'},
+        { cols: 1, rows: 1, url: '../../../assets/buildings.jpeg' ,
+          list1: 'Relevé topographique',
+          list2: 'Bornage',
+          list3: 'Division',
+          list4: 'Etude de voirie',
+          list5: 'Etat des lieux',
+          list6: 'Expertise immobilière',
+          list7: 'Implantation de batiment'
+        },
+        { cols: 1, rows: 1 , url: '../../../assets/call.jpg',
+          list1: 'Tel: +32 495 56 02 85',
+          list2: 'Mail: pters.kevin@gmail.com',
+          list3: 'Adresse: Voie de l\'Air Pur 222, 4052 Beaufays'
+        }
       ];
     })
   );
