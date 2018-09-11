523c14d2614b87ff29085fe2c3c85219328e8dd5
ku714 <jerome.wilmart@clearstream.com>
2018-09-11 10:55:48 +0200

    mobile

--- src/app/materials/dashboard/dashboard.component.ts
+++ src/app/materials/dashboard/dashboard.component.ts
@@ -14,7 +14,7 @@ export class DashboardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { cols: 1, rows: 1, url: '../../../assets/banner.jpg' },
+          { cols: 2, rows: 1, url: '../../../assets/mobilebanner.png' },
           { cols: 1, rows: 1 , url: '../../../assets/buildings.jpeg',
             list1: 'Relevé topographique',
             list2: 'Bornage',
@@ -22,12 +22,18 @@ export class DashboardComponent {
             list4: 'Etude de voirie',
             list5: 'Etat des lieux',
             list6: 'Expertise immobilière',
-            list7: 'Implantation de batiment'
+            list7: 'Implantation de batiment',
+            fontSize: 'medium',
+            fontWeight: '800',
+            bgc: 'white'
             },
           { cols: 1, rows: 1 , url: '../../../assets/call.jpg',
             list1: 'Tel: +32 495 56 02 85',
             list2: 'Mail: pters.kevin@gmail.com',
-            list3: 'Adresse: Voie de l\'Air Pur 222, 4052 Beaufays'
+            list3: 'Adresse: Voie de l\'Air Pur 222, 4052 Beaufays',
+            fontSize: 'medium',
+            fontWeight: '800',
+            bgc: 'white'
           }        ];
       }
 
@@ -40,17 +46,20 @@ export class DashboardComponent {
           list4: 'Etude de voirie',
           list5: 'Etat des lieux',
           list6: 'Expertise immobilière',
-          list7: 'Implantation de batiment'
+          list7: 'Implantation de batiment',
+          fontSize: 'x-large'
         },
         { cols: 1, rows: 1 , url: '../../../assets/call.jpg',
           list1: 'Tel: +32 495 56 02 85',
           list2: 'Mail: pters.kevin@gmail.com',
           list3: 'Adresse: Voie de l\'Air Pur 222, 4052 Beaufays',
-          type: 'contact'
+          type: 'contact',
+          fontSize: 'x-large'
         }
       ];
     })
   );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
 }
