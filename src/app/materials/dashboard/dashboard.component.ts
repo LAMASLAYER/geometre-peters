60f0eec4a5430831eadbfebcf1c20dd93f07b541
ku714 <jerome.wilmart@clearstream.com>
2019-01-17 16:45:34 +0100

    new

--- src/app/materials/dashboard/dashboard.component.ts
+++ src/app/materials/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
@@ -7,68 +7,71 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  get services(): boolean {
+    return this._services;
+  }
 
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { cols: 1, rows: 1 , url: '../../../assets/buildings.jpeg',
-            list1: 'Relevé topographique',
-            list2: 'Bornage',
-            list3: 'Division',
-            list4: 'Etude de voirie',
-            list5: 'Etat des lieux',
-            list6: 'Expertise immobilière',
-            list7: 'Implantation de batiment',
-            fontSize: 'medium',
-            fontWeight: '800',
-            bgc: 'white',
-            text: 'center',
-            padding: '0',
-            type: 'other',
-          },
-          { cols: 1, rows: 1 , url: '../../../assets/call.jpg',
-            list1: 'Téléphone',
-            list2: 'Mail',
-            list3: 'Voie de l\'Air Pur 222, 4052 Beaufays',
-            fontSize: 'medium',
-            fontWeight: '800',
-            bgc: 'white',
-            text: 'center',
-            padding: '0',
-            cursor: 'pointer',
-            opacity: '0.5',
-            type: 'contact',
-          }        ];
-      }
+  set services(value: boolean) {
+    this._services = value;
+  }
+  get about(): boolean {
+    return this._about;
+  }
 
-      return [
-        { cols: 1, rows: 1, url: '../../../assets/buildings.jpeg' ,
-          list1: 'Relevé topographique',
-          list2: 'Bornage',
-          list3: 'Division',
-          list4: 'Etude de voirie',
-          list5: 'Etat des lieux',
-          list6: 'Expertise immobilière',
-          list7: 'Implantation de batiment',
-          fontSize: 'x-large',
-          text: 'right',
-          type: 'other',
-        },
-        { cols: 1, rows: 1 , url: '../../../assets/call.jpg',
-          list1: 'Tel: +32 495 56 02 85',
-          list2: 'Mail: pters.kevin@gmail.com',
-          list3: 'Adresse: Voie de l\'Air Pur 222, 4052 Beaufays',
-          type: 'contact',
-          fontSize: 'x-large',
-          text: 'right'
-        }
-      ];
-    })
-  );
+  set about(value: boolean) {
+    this._about = value;
+  }
+  get main(): boolean {
+    return this._main;
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  set main(value: boolean) {
+    this._main = value;
+  }
+  get dropdown(): boolean {
+    return this._dropdown;
+  }
 
+  set dropdown(value: boolean) {
+    this._dropdown = value;
+  }
+  get openMenu(): boolean {
+    return this._openMenu;
+  }
+
+  set openMenu(value: boolean) {
+    this._openMenu = value;
+  }
+  get mobile(): boolean {
+    return this._mobile;
+  }
+
+  set mobile(value: boolean) {
+    this._mobile = value;
+  }
+
+  private _mobile: boolean;
+  private _openMenu: boolean;
+  private _dropdown: boolean;
+  private _main: boolean;
+  private _about: boolean;
+  private _services: boolean;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this._main = true;
+    const w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementsByTagName('body')[0],
+      x = w.innerWidth || e.clientWidth || g.clientWidth,
+      y = w.innerHeight || e.clientHeight || g.clientHeight;
+    if (x > 720) {
+      this._mobile = true;
+    } else {
+      this._mobile = false;
+    }
+  }
 }
